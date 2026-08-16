572a85a568cf078a525845d357a14e7fbadb587c
madenjanna-ui <madenjanna@gmail.com>
2026-08-17 00:18:00 +0400

    Update config.js

--- client/js/config.js
+++ client/js/config.js
@@ -3,5 +3,5 @@
 // GitHub Pages: set this to your HTTPS Family server, for example:
 // https://family.example.com
 window.FAMILY_CONFIG = {
-    API_BASE: ""
+    API_BASE: "https://family-server-production-8db2.up.railway.app"
 };
