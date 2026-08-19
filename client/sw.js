586c496643789db0e84064c1d0e1b3f7a160f92f
madenjanna-ui <madenjanna@gmail.com>
2026-08-19 11:32:27 +0400

    Add files via upload

--- client/sw.js
+++ client/sw.js
@@ -1,13 +1,13 @@
-const CACHE = "family-shell-v6-2-2";
+const CACHE = "family-shell-v6-2-3";
 const SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
   "./css/style.css?v=23",
   "./js/config.js?v=3",
-  "./js/api.js?v=23",
+  "./js/api.js?v=24",
   "./js/auth.js?v=10",
-  "./js/app.js?v=23",
+  "./js/app.js?v=24",
   "./assets/icon-180.png",
   "./assets/icon-512.png",
   "./js/media.js?v=2"
