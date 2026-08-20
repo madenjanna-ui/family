b7f0aafcd2ed854c63bfd7ec9fc295517f25a037
madenjanna-ui <madenjanna@gmail.com>
2026-08-20 10:04:14 +0400

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -17,6 +17,46 @@ const App = {
     usersCache: [],
     replyTarget: null,
     favorites: JSON.parse(localStorage.getItem("FamilyFavorites") || "[]"),
+ appendMessage(message, global=false, otherId=null){
+
+    const box = document.getElementById(
+        global ? "messages" : "privateMessages"
+    );
+
+    if(!box) return;
+
+    const input =
+        document.activeElement;
+
+    const typing =
+        input &&
+        input.tagName === "INPUT" &&
+        input.value.length > 0;
+
+
+    const nearBottom =
+        box.scrollHeight -
+        box.scrollTop -
+        box.clientHeight < 120;
+
+
+    box.insertAdjacentHTML(
+        "beforeend",
+        this.messageHtml(
+            message,
+            global,
+            otherId
+        )
+    );
+
+
+    // только если пользователь уже был внизу
+    if(nearBottom && !typing){
+        this.scrollMessages(
+            global ? "messages" : "privateMessages"
+        );
+    }
+}
 
     async start() {
         this.applyAppearance();
@@ -442,46 +482,7 @@ await API.pushSubscribe(subscription.toJSON());
         return messages;
     },
     getPrivateChatId(a,b){return [Number(a),Number(b)].sort((x,y)=>x-y).join("_");},
-  appendMessage(message, global=false, otherId=null){
-
-    const box = document.getElementById(
-        global ? "messages" : "privateMessages"
-    );
-
-    if(!box) return;
-
-    const input =
-        document.activeElement;
-
-    const typing =
-        input &&
-        input.tagName === "INPUT" &&
-        input.value.length > 0;
-
-
-    const nearBottom =
-        box.scrollHeight -
-        box.scrollTop -
-        box.clientHeight < 120;
-
-
-    box.insertAdjacentHTML(
-        "beforeend",
-        this.messageHtml(
-            message,
-            global,
-            otherId
-        )
-    );
-
-
-    // только если пользователь уже был внизу
-    if(nearBottom && !typing){
-        this.scrollMessages(
-            global ? "messages" : "privateMessages"
-        );
-    }
-}
+ 
     esc(v){return String(v??"").replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/"/g,"&quot;").replace(/'/g,"&#039;");},
     attr(v){return this.esc(v);}
 };
